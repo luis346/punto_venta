d749cb21d39419d4efacfa0b4161e2c10ac58de0
Luis Lovera <kande71115@gmial.com>
2025-06-04 15:47:27 -0600

    cambio en db

--- punto_venta/settings.py
+++ punto_venta/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +78,9 @@ WSGI_APPLICATION = 'punto_venta.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:lorax21_0@localhost:5432/punto_venta_db',
+    )
 }
 
 
