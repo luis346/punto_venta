e3fb2ff9bd444bb2ba95a35c2f93e5c07598b347
luis346 <71810182+luis346@users.noreply.github.com>
2026-02-17 18:08:10 -0600

    Sistema auto inicializable producción

---
+++ core/signals.py
@@ -0,0 +1,39 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from core.models import Sucursal
+
+User = get_user_model()
+
+
+@receiver(post_migrate)
+def inicializar_sistema(sender, **kwargs):
+
+    # =========================
+    # 1️⃣ Crear sucursal si no existe
+    # =========================
+    if not Sucursal.objects.exists():
+        sucursal = Sucursal.objects.create(
+            nombre="Sucursal Principal",
+            activa=True
+        )
+        print("Sucursal Principal creada ✅")
+    else:
+        sucursal = Sucursal.objects.first()
+
+    # =========================
+    # 2️⃣ Crear admin si no existe
+    # =========================
+    if not User.objects.filter(rol="ADMIN").exists():
+        admin = User.objects.create_superuser(
+            username="admin",
+            email="admin@admin.com",
+            password="Admin1234"
+        )
+
+        # Si tu modelo tiene campo sucursal
+        if hasattr(admin, "sucursal"):
+            admin.sucursal = sucursal
+            admin.save()
+
+        print("Usuario admin creado ✅")
